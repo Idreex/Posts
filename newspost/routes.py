55400605f1b59c60457efca95062b6075629f4a7
Idreex <idreesademola07@gmail.com>
2023-12-14 00:40:50 +0100

    picture updates

--- newspost/routes.py
+++ newspost/routes.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from newspost import app,db
-from flask import render_template, redirect, flash, request
+from flask import render_template, redirect, flash, url_for, request
 from newspost.form import RegistrationForm, LoginForm, UpdateAccountForm
 from newspost.model import User, Post
 from newspost import bcrypt
@@ -92,21 +92,29 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root.path, 'static/img', + picture_fn)
+    picture_path = os.path.join(app.root_path, 'static/img', picture_fn)
     output_size = (125,125)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
     i.save(picture_path)
     return picture_fn
 
-@app.route('/account')
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
-        db.commit()
-        flash('You account has been updated')
-    return render_template('account.html', title='account', form=form)
-
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='img/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
