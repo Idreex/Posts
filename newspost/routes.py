563fce5f451542e51b58a965daa812721f270f8a
Idreex <idreesademola07@gmail.com>
2023-12-21 23:41:45 +0100

    alh

--- newspost/routes.py
+++ newspost/routes.py
@@ -17,13 +17,12 @@ from PIL import Image
 @login_required
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=3)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=3, page=page)
 
-    return render_template('home.html', title='Home', posts=posts, page=page)
-
-
+    return render_template('home.html', title='Home', posts=posts)
 
 
+ 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
